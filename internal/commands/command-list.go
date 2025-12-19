a0dfb6ff376559421ccec8bf42e00e434689cb54
Michael Bond <Michael.Bond@move.com>
2025-12-18 21:02:47 -0500

    feat: move commands (aliases)

--- internal/commands/command-list.go
+++ internal/commands/command-list.go
@@ -6,6 +6,12 @@ func init() {
 	CommandList = map[string]Command{
 		"look":  Look,
 		"move":  Move,
+		"north": MoveNorth,
+		"n":     MoveNorth,
+		"south": MoveSouth,
+		"east":  MoveEast,
+		"west":  MoveWest,
+		"up":    MoveUp,
 		"say":   Say,
 		"/exit": Exit,
 		"/help": Help,
--- internal/commands/move.go
+++ internal/commands/move.go
@@ -14,6 +14,48 @@ var Move = Command{
 	Execute:     moveFunction,
 }
 
+var MoveNorth = Command{
+	Name:        "north",
+	Description: "Move to the north.",
+	Help:        "Usage: north <direction>\nMove through the north exit",
+	Execute:     moveNorthFunction,
+}
+
+var MoveSouth = Command{
+	Name:        "south",
+	Description: "Move to the south.",
+	Help:        "Usage: south <direction>\nMove through the south exit",
+	Execute:     moveSouthFunction,
+}
+
+var MoveEast = Command{
+	Name:        "east",
+	Description: "Move to the east.",
+	Help:        "Usage: east <direction>\nMove through the east exit",
+	Execute:     moveEastFunction,
+}
+
+var MoveWest = Command{
+	Name:        "west",
+	Description: "Move to the west.",
+	Help:        "Usage: west <direction>\nMove through the west exit",
+	Execute:     moveWestFunction,
+}
+
+var MoveUp = Command{
+	Name:        "up",
+	Description: "Move up.",
+	Help:        "Usage: up <direction>\nMove through the up exit",
+	Execute:     moveUpFunction,
+}
+
+var MoveDown = Command{
+	Name:        "down",
+	Description: "Move down.",
+	Help:        "Usage: down <direction>\nMove through the down exit",
+	Execute:     moveDownFunction,
+}
+
 func moveFunction(player *player.Player, rooms *locations.RoomsById, cmd ParsedCommand) (CommandReturn, error) {
 	room, roomExists := locations.GetRoomById(rooms, player.Location)
 	if !roomExists {
@@ -31,4 +73,34 @@ func moveFunction(player *player.Player, rooms *locations.RoomsById, cmd ParsedC
 		Message: "You move " + cmd.Args[0] + "." + "\n" + locations.DisplayRoom(rooms, player.Location),
 	}
 	return ret, nil
+}
+
+func moveNorthFunction(player *player.Player, rooms *locations.RoomsById, cmd ParsedCommand) (CommandReturn, error) {
+	cmd.Args = []string{"north"}
+	return moveFunction(player, rooms, cmd)
+}
+
+func moveSouthFunction(player *player.Player, rooms *locations.RoomsById, cmd ParsedCommand) (CommandReturn, error) {
+	cmd.Args = []string{"south"}
+	return moveFunction(player, rooms, cmd)
+}
+
+func moveEastFunction(player *player.Player, rooms *locations.RoomsById, cmd ParsedCommand) (CommandReturn, error) {
+	cmd.Args = []string{"east"}
+	return moveFunction(player, rooms, cmd)
+}
+
+func moveWestFunction(player *player.Player, rooms *locations.RoomsById, cmd ParsedCommand) (CommandReturn, error) {
+	cmd.Args = []string{"west"}
+	return moveFunction(player, rooms, cmd)
+}
+
+func moveUpFunction(player *player.Player, rooms *locations.RoomsById, cmd ParsedCommand) (CommandReturn, error) {
+	cmd.Args = []string{"up"}
+	return moveFunction(player, rooms, cmd)
+}
+
+func moveDownFunction(player *player.Player, rooms *locations.RoomsById, cmd ParsedCommand) (CommandReturn, error) {
+	cmd.Args = []string{"down"}
+	return moveFunction(player, rooms, cmd)
 }
